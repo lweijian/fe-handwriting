73643719880d9556058b49831cd29b65f24118c8
liangweijian2 <liangweijian2@yy.com>
2022-03-30 01:14:10 +0800

    事件发布订阅

---
+++ js题/EventEmitter.js
@@ -0,0 +1,57 @@
+//事件发布订阅
+class EventEmitter {
+    constructor(){
+        this.event={}
+    }
+    on(eventName,callback){
+    if(this.event.hasOwnProperty(eventName)){
+        this.event[eventName].push(callback)
+    }else {
+        this.event[eventName]=[callback]
+    }
+    }
+    off(eventName,callback){
+        if(!this.event.hasOwnProperty(eventName)){
+            return true;
+        }else {
+            this.event[eventName]=this.event[eventName].filter(fn=>{
+                return fn!==callback
+            })
+        }
+    }
+    emit(eventName,...args){
+   const fns= this.event[eventName]??[]
+        for (const fn of fns) {
+            Reflect.apply(fn,this,args)
+        }
+    }
+    once(eventName,callback){
+        const fn=()=>{
+            callback()
+            this.off(eventName,fn)
+        }
+        this.on(eventName,fn)
+    }
+
+}
+// 使用如下
+const event = new EventEmitter();
+
+const handle = (...rest) => {
+  console.log(rest);
+};
+
+event.on("click", handle);
+
+event.emit("click", 1, 2, 3, 4);
+//
+event.off("click", handle);
+//
+event.emit("click", 1, 2);
+
+event.once("dbClick", () => {
+  console.log(123456);
+});
+event.emit("dbClick");
+event.emit("dbClick");
+
